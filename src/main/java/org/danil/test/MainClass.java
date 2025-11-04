f39834184cdb44a183eed051679083c4c248880e
Fort163 <Front-91@mail.ru>
2025-11-04 19:14:51 +0400

    first task

--- src/main/java/org/danil/test/MainClass.java
+++ src/main/java/org/danil/test/MainClass.java
@@ -1,9 +1,24 @@
 package org.danil.test;
 
+import java.util.Scanner;
+
 public class MainClass {
 
+    /*
+    Дописать программу чтобы она говорила кому нельзя пить алкоголь
+     */
+
     public static void main(String args[]){
-        System.out.println("Здарова Зверь!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите имя : ");
+        String name = scanner.next();
+        System.out.println("Введите возраст : ");
+        int age = scanner.nextInt();
+
+        if(age > 18){
+            System.out.println("Тебе можно пить бухлишко : "+ name);
+        }
+
     }
 
 }
